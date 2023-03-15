5889c535dbd5be0479b8a4a15c91a98daef1c49f
Vladisav Mayakin <vlad99may@gmail.com>
2023-03-15 22:15:56 +0300

    Organization of texts is done

--- src/application.js
+++ src/application.js
@@ -1,8 +1,24 @@
-import { string } from 'yup';
+import { string, setLocale } from 'yup';
 import onChange from 'on-change';
 import render from './view';
+import i18next from 'i18next';
+import ru from './locales/ru';
 
 const app = () => {
+  const i18nextInstance = i18next.createInstance();
+  i18nextInstance.init({
+    lng: 'ru',
+    debug: false,
+    resources: {
+      ru,
+    },
+  });
+
+  setLocale({
+    mixed: { notOneOf: 'errors.alreadyExists' },
+    string: { url: 'errors.invalidUrl' },
+  });
+
   const initialState = {
     form: {
       errors: [],
@@ -16,15 +32,16 @@ const app = () => {
     feedback: document.querySelector('.feedback'),
   };
 
-  const watchedState = onChange(initialState, render(elements));
+  const watchedState = onChange(
+    initialState,
+    render(elements, i18nextInstance)
+  );
 
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const url = formData.get('url').trim();
-    const schema = string()
-      .url('Ссылка должна быть валидным URL')
-      .notOneOf(initialState.feeds, 'RSS уже существует');
+    const schema = string().url().notOneOf(initialState.feeds);
 
     schema
       .validate(url)
--- src/view.js
+++ src/view.js
@@ -1,17 +1,17 @@
-const render = (elements) => (path, value) => {
+const render = (elements, i18nextInstance) => (path, value) => {
   switch (path) {
     case 'form.errors':
       elements.feedback.textContent = '';
       elements.urlInput.classList.add('is-invalid');
       elements.feedback.classList.add('text-danger');
-      elements.feedback.textContent = value[0].message;
+      elements.feedback.textContent = i18nextInstance.t(value[0].message);
       break;
     case 'feeds':
       elements.feedback.textContent = '';
       elements.urlInput.classList.remove('is-invalid');
       elements.feedback.classList.remove('text-danger');
       elements.feedback.classList.add('text-success');
-      elements.feedback.textContent = 'RSS успешно загружен';
+      elements.feedback.textContent = i18nextInstance.t('validUrl');
       elements.form.reset();
       elements.urlInput.focus();
       break;
